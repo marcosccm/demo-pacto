af41d9686097258ebdb5a6a47db51108e603df57
Marcos Castilho <marcosccm@gmail.com>
2014-10-07 18:34:23 +0100

    Add contract for the spec

--- accounts_spec.rb
+++ accounts_spec.rb
@@ -1,12 +1,17 @@
 require_relative './accounts'
 require 'json'
 require 'rack/test'
+require 'pacto'
 
 describe 'accounts service' do
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
+  end
+
+  before do
+    Pacto.load_contracts('contracts', 'http://localhost:4567').stub_all
   end
 
   it 'works well' do
